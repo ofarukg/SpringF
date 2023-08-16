10a3b75a5b8904809725263ece7f0e262904135e
ofarukg <130508202+ofarukg@users.noreply.github.com>
2023-08-16 18:07:54 +0300

    Add files via upload

---
+++ 01-spring-boot-spring-mvc-security-default/src/test/java/com/luv2code/springboot/security/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
